b88fe047ea1ddaba8cc8f379c2c3e2b7620e00fd
Diego Bortolucci <bortolucci.diego@gmail.com>
2020-06-15 09:31:16 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -104,3 +104,7 @@ Route::get('/cliente', ['uses'=>'ClienteController@index','as'=>'cliente.index']
 Route::get('/cliente/adicionar',['uses'=>'ClienteController@adicionar','as'=>'cliente.adicionar']);
 
 Route::post('/cliente/salvar',['uses'=>'ClienteController@salvar','as'=>'cliente.salvar']);
+
+Route::get('/cliente/editar/{id}', ['uses'=>'ClienteController@editar', 'as'=>'cliente.editar']);
+
+Route::put('/cliente/atualizar/{id}', ['uses'=>'ClienteController@atualizar', 'as'=>'cliente.atualizar']);
