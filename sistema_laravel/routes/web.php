2a7c84e5d1355ace15fabd3b784f65d021bb56c8
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-07-12 23:38:06 -0300

    routes

--- sistema_laravel/routes/web.php
+++ sistema_laravel/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, Mundo! :)';
+});
+
+Route::get('/sobre', function () {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
