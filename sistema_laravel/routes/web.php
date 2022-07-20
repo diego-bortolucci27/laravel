5be6ef743105167951590eb8d0413b37d86544d8
Diego Bortolucci <bortolucci.diego@gmail.com>
2022-07-20 20:36:20 -0300

    update

--- sistema_laravel/routes/web.php
+++ sistema_laravel/routes/web.php
@@ -13,11 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');//Quando você utiliza o Controller ao invérs de passar uma function, passe uma string com o Nome do Controller e o @ com o nome da função dentro da Class Controller.
+Route::get('/', 'PrincipalController@principal')->name('site.index');//Quando você utiliza o Controller ao invérs de passar uma function, passe uma string com o Nome do Controller e o @ com o nome da função dentro da Class Controller.
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/sobrenos', 'SobreNosController@sobreNos');
+Route::get('/sobrenos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){  return 'Produtos'; })->name('app.produtos');
+});
+
 
 // nome, categoria, assunto, mensagem
 // Para indicar parâmetros em uma rota, basta você colocar chaves -> Route::get('/contato/{}' ..... ) com o ? quer dizer q o parametro é opcional
@@ -31,3 +40,15 @@ Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{msg?}',
         {
             echo 'Estamos aqui: ' . $nome . ' - ' . $categoria . ' - ' . $assunto . ' - ' . $msg;
         });
+
+//Redirecionamento de Rotas
+Route::get('/rota1', function(){ echo 'Rota 1'; })->name('site.rota1');
+Route::get('/rota2', function(){ return redirect()->route('site.rota1'); })->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
+
+
+//Rota de Fallback - Rota/página de erro - de rota inexistente
+Route::fallback(function (){
+    echo 'Página inexistente.  <a href="'.route('site.index').'">Clique aqui</a>; para ir a pagina inicial'; 
+});
